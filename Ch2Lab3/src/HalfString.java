7eccb9b6e5676b885c50cee2a8c2377360373fc9
SushiRoll55 <73790688+SushiRoll55@users.noreply.github.com>
2023-09-19 15:37:18 -0700

    Completed ch2lab3

--- Ch2Lab3/src/HalfString.java
+++ Ch2Lab3/src/HalfString.java
@@ -1,8 +1,30 @@
+//Knox, Caden
+//September 19, 2023
+//CS A170
+//Chapter 2 Lab 3
+
+import java.util.Scanner;
 
 public class HalfString {
 
 	public static void main(String[] args) {
+		//Initialize scanner
+		Scanner n = new Scanner(System.in);
 		
+		//Prompt for and receive string
+		System.out.print("Please enter an even length string: ");
+		String str = n.nextLine();
+		
+		//Check if the string length is even
+		if (str.length() % 2 == 0)
+		{
+			//Cut str in half and print
+			str = str.substring(0, str.length()/2);
+			System.out.print(str);
+		}
+		else
+		{
+			System.out.print("String is not even, try again");
+		}
 	}
-
 }
--- Ch2Lab3/src/WordConcat.java
+++ Ch2Lab3/src/WordConcat.java
@@ -1,8 +1,31 @@
+//Knox, Caden
+//September 19, 2023
+//CS A170
+//Chapter 2 Lab 3
+
+import java.util.Scanner;
 
 public class WordConcat {
 
 	public static void main(String[] args) {
+		//Initialize scanner
+		Scanner n = new Scanner(System.in);
 		
+		//Prompt for and receive a string
+		System.out.print("Enter a string: ");
+		String str = n.nextLine();
+		
+		//Get first and last chars
+		char firstChar = str.charAt(0);
+		char lastChar = str.charAt(str.length()-1);
+		
+		//Replace first character
+		str = lastChar + str.substring(1, str.length());
+		
+		//Replace second character
+		str = str.substring(0, str.length()-1) + firstChar;
+		
+		//Print result
+		System.out.print(str);
 	}
-
 }
